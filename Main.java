4a045f653aeee7c1a5dcf83cf785404e95f6ab35
dinorah <86006528+dinorahfariasc@users.noreply.github.com>
2024-10-08 19:43:45 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,33 @@
+//exercicios
+
+// import java.util.Scanner;
+
+// public class Main {
+//     // lado do triangulo, recebe 3 lados e diz se é isoceles, equilatero, escaleno
+    
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         System.out.println("Digite o lado a do triangulo: ");
+//         int a = sc.nextInt();
+//         System.out.println("Digite o lado b do triangulo: ");
+//         int b = sc.nextInt();
+//         System.out.println("Digite o lado c do triangulo: ");
+//         int c = sc.nextInt();
+        
+//         if(a == b && b == c){
+//             System.out.println("Equilatero");
+        
+//         }else if(a == b || b == c || a == c){
+//             System.out.println("Isoceles");
+        
+//         }else{
+//             System.out.println("Escaleno");
+//         }
+//     }
+// }
+    
+//=========================================================================================================
+
+
+
