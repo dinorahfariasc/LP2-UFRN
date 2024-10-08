28cdeb389e9b0bccc93ef8490fcb3b0d87e382ab
dinorah <86006528+dinorahfariasc@users.noreply.github.com>
2024-10-08 20:02:21 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -29,5 +29,32 @@
     
 //=========================================================================================================
 
+// public class Main {
+//     // escolhe entre soma, subtração, multiplicação, divisão e exponenciação.
+
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+        
+//         System.out.println("Digite o primeiro número: ");
+//         int a = sc.nextInt();
+//         System.out.println("Digite o segundo número: ");
+//         int b = sc.nextInt();
+        
+//         System.out.println("Digite a operação desejada: ");
+//         String operacao = sc.next();
+        
+//         switch (operacao) {
+//             case "1" -> System.out.println("Soma: " + (a + b));
+//             case "2" -> System.out.println("Subtração: " + (a - b));
+//             case "3" -> System.out.println("Multiplicação: " + (a * b));
+//             case "4" -> System.out.println("Divisão: " + (a / b));
+//             case "5" -> System.out.println("Exponenciação: " + (Math.pow(a, b)));
+            
+//             default -> {
+//             }
+//         }
+//     }
+    
+// }
 
 
