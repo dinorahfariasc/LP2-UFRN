9119a60bb9938380e8849d7b0b73eb63faef5c75
dinorah <86006528+dinorahfariasc@users.noreply.github.com>
2024-10-18 21:15:37 -0300

    Create VerificadoeAnoBissexto.java

---
+++ Lista2/VerificadoeAnoBissexto.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class VerificadorAnoBissexto {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um ano: ");
+        int ano = scanner.nextInt();
+
+        if (isBissexto(ano)) {
+            System.out.println(ano + " é um ano bissexto.");
+        } else {
+            System.out.println(ano + " não é um ano bissexto.");
+        }
+
+        scanner.close();
+    }
+
+    private static boolean isBissexto(int ano) {
+        return (ano % 4 == 0 && ano % 100 != 0) || (ano % 400 == 0);
+    }
+}
